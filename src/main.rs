116f435e18755b9bef1488521b32d174d85443c2
Jeremy Andrews <jeremy@tag1consulting.com>
2019-12-27 17:33:47 +0100

    actix hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap();
 }
